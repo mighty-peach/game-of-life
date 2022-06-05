86c27bac188089028c951e5ebd36314a31a66baa
mighty_peach <mighty_peach@fastmail.com>
2022-06-05 20:17:33 +0700

    add responsibility to game field

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 // TODO: Or there is too much lines with window resize, or clearcolor doesn't happened
 use bevy::{
     core_pipeline::ClearColor,
-    math::{Vec2, Vec3},
+    math::Vec3,
     prelude::{
-        default, App, Commands, CoreStage, Deref, DerefMut, OrthographicCameraBundle, Plugin, Res,
-        ResMut, SystemSet, Transform,
+        default, App, Commands, Component, CoreStage, Deref, DerefMut, Entity,
+        OrthographicCameraBundle, Plugin, Query, Res, ResMut, SystemSet, Transform,
     },
     sprite::{Sprite, SpriteBundle},
     window::{WindowDescriptor, Windows},
@@ -15,10 +15,7 @@ use config::{BACKGROUND_COLOR, MAIN_COLOR, WINDOW_PADDING, WINDOW_SIZE};
 mod config;
 
 fn main() {
-    App::new()
-        .add_plugin(GameSetup)
-        .add_system(setup_field)
-        .run();
+    App::new().add_plugin(GameSetup).run();
 }
 
 struct GameSetup;
@@ -33,11 +30,14 @@ impl Plugin for GameSetup {
                 ..default()
             })
             .add_startup_system(setup_camera)
+            .add_startup_system(setup_field)
+            .insert_resource(Lines::default())
             .add_plugins(DefaultPlugins)
-            .insert_resource(StepSize(10.0))
             .add_system_set_to_stage(
                 CoreStage::PostUpdate,
-                SystemSet::new().with_system(size_scaling),
+                SystemSet::new()
+                    .with_system(position_translation)
+                    .with_system(size_scaling),
             );
     }
 }
@@ -46,66 +46,95 @@ fn setup_camera(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
 
-#[derive(Default, Deref, DerefMut)]
-struct StepSize(f32);
-
-fn size_scaling(windows: Res<Windows>, mut step_size: ResMut<StepSize>) {
-    let window = windows
-        .get_primary()
-        .expect("error while getting primary window");
-
-    *step_size = StepSize((window.width() - (WINDOW_PADDING * 2.)) / 30.);
+#[derive(Component)]
+struct Position {
+    x: f32,
+    y: f32,
 }
 
-/// Spawn field, filled by lines, which create
-/// an ordered bunch of squares
-///
-/// TODO: move to separete file?
-fn setup_field(mut commands: Commands, step_size: Res<StepSize>) {
-    const FIELD_SIZE: f32 = WINDOW_SIZE - WINDOW_PADDING;
-    let half_of_field: i32 = (FIELD_SIZE / step_size.0 / 2.0) as i32;
-    // Store lines with predefined capacity that equals lines count
-    let mut lines: Vec<SpriteBundle> = Vec::with_capacity(
-        (half_of_field / 2 + 2)
-            .try_into()
-            .expect("converting i32 to usize error"),
-    );
+#[derive(Component)]
+struct LineProperty {
+    is_vertical: bool,
+}
+
+fn position_translation(windows: Res<Windows>, mut q: Query<(&Position, &mut Transform)>) {
+    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
+        let tile_size = bound_window / bound_game;
+
+        pos * tile_size
+    }
+
+    let window = windows.get_primary().unwrap();
+    let padding_size = WINDOW_PADDING * 2.0;
+    for (pos, mut transform) in q.iter_mut() {
+        transform.translation = Vec3::new(
+            convert(pos.x as f32, window.width() - padding_size, 30.0),
+            convert(pos.y as f32, window.width() - padding_size, 30.0),
+            0.0,
+        )
+    }
+}
+
+fn size_scaling(windows: Res<Windows>, mut q: Query<(&LineProperty, &mut Transform)>) {
+    let window = windows.get_primary().unwrap();
+    let padding_size = WINDOW_PADDING * 2.0;
+    for (props, mut transform) in q.iter_mut() {
+        if props.is_vertical {
+            transform.scale = Vec3::new(1.0, (window.width() as f32) - padding_size, 1.0);
+        } else {
+            transform.scale = Vec3::new((window.width() as f32) - padding_size, 1.0, 1.0);
+        }
+    }
+}
+
+#[derive(Default, Deref, DerefMut)]
+struct Lines(Vec<Entity>);
+
+fn setup_field(mut commands: Commands, mut lines: ResMut<Lines>) {
+    let cells_count = 30.0;
+    let half_of_field_lines_count = (cells_count / 2.) as i32;
 
     // calculates steps for iterator for creating lines
     // at first it calculates centre of field and than it finds
     // starting position of lines and end
-    let calculate_range = || (-1 * half_of_field, half_of_field + 1);
+    let calculate_range = || {
+        (
+            -1 * half_of_field_lines_count,
+            half_of_field_lines_count + 1,
+        )
+    };
     let range = calculate_range();
 
-    // TODO: refactor this, initialise global structure Size and add system that calculates
-    // size depend of window size, padding and cells_count
-    fn get_line_sprite(position_x: f32, position_y: f32, is_vertical: bool) -> SpriteBundle {
-        let size: Vec2 = match is_vertical {
-            true => Vec2::new(FIELD_SIZE, 1.0),
-            false => Vec2::new(1.0, FIELD_SIZE),
-        };
-
-        SpriteBundle {
-            sprite: Sprite {
-                color: MAIN_COLOR,
-                custom_size: Some(size),
-                ..default()
-            },
-            transform: Transform {
-                translation: Vec3::new(position_x, position_y, 0.0),
-                ..default()
-            },
-            ..default()
-        }
-    }
-
-    // because sprites are aligned from centre
-    // left edge of window is window_size / 2 * -1
-    // I use 16 in order to have right and top border
     for i in range.0..range.1 {
-        lines.push(get_line_sprite(i as f32 * step_size.0, 0.0, false));
-        lines.push(get_line_sprite(0.0, i as f32 * step_size.0, true));
+        *lines = Lines(vec![
+            commands
+                .spawn_bundle(SpriteBundle {
+                    sprite: Sprite {
+                        color: MAIN_COLOR,
+                        ..default()
+                    },
+                    ..default()
+                })
+                .insert(Position {
+                    x: i as f32,
+                    y: 0.0,
+                })
+                .insert(LineProperty { is_vertical: true })
+                .id(),
+            commands
+                .spawn_bundle(SpriteBundle {
+                    sprite: Sprite {
+                        color: MAIN_COLOR,
+                        ..default()
+                    },
+                    ..default()
+                })
+                .insert(LineProperty { is_vertical: false })
+                .insert(Position {
+                    x: 0.0,
+                    y: i as f32,
+                })
+                .id(),
+        ]);
     }
-
-    commands.spawn_batch(lines);
 }
